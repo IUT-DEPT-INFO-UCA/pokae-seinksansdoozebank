4eba76d4aca72abcd721d2bbbf6ebf3fcf95384e
Arcsti <104559304+Arcsti@users.noreply.github.com>
2022-05-07 16:39:50 +0200

    Update Pokemon.java

--- src/gestionPokemon/Pokemon.java
+++ src/gestionPokemon/Pokemon.java
@@ -8,7 +8,7 @@ public class Pokemon {
     public double xp; // Points d'exp�riences
     public Espece espPoke;
     public Capacite[] listeCapacite = new Capacite[4];
-
+    
     // Stats sp�cifiques :
     public int atk;
     public int def;
@@ -23,14 +23,14 @@ public class Pokemon {
     private int evVit;
     private int evSpe;
     private int evPv;
-
+    
     // Valeurs  determinantes == puissance native
     private int dvAtq;
     private int dvDef;
     private int dvVit;
     private int dvSpe;
     private int dvPv;
-
+    
     //historique et effet des capacite
     private Capacite derniereCapciteUtilisee;
     private double deniersDegatsSubits;
@@ -60,42 +60,42 @@ public class Pokemon {
     public void mettreId(int id) {
         this.id = id;
     }
-
+    
     public Capacite obtenirDerniereCapaUtilisee() {
-        return this.derniereCapciteUtilisee;
+    	return this.derniereCapciteUtilisee;
     }
 
     public double obtenirDeniersDegatsSubits() {
-        return this.deniersDegatsSubits;
+    	return this.deniersDegatsSubits;
     }
     public double obtenirAvantDeniersDegatsSubits() {
-        return this.avantDeniersDegatsSubits;
+    	return this.avantDeniersDegatsSubits;
     }
+    
+	public int obtenirNombreDeToursAvantAttaque() {
+		return nombreDeToursAvantAttaque;
+	}
+	
+	public void updateNombreDeToursAvantAttaque() {
+		if(this.nombreDeToursAvantAttaque>0) {
+			this.nombreDeToursAvantAttaque-=1;
+		}
+	}
 
-    public int obtenirNombreDeToursAvantAttaque() {
-        return nombreDeToursAvantAttaque;
-    }
-
-    public void updateNombreDeToursAvantAttaque() {
-        if(this.nombreDeToursAvantAttaque>0) {
-            this.nombreDeToursAvantAttaque-=1;
-        }
-    }
-
-    public void mettreNombreDeToursAvantAttaqueA(int nombreDeToursAvantAttaque) {
-        this.nombreDeToursAvantAttaque = nombreDeToursAvantAttaque;
-    }
-
+	public void mettreNombreDeToursAvantAttaqueA(int nombreDeToursAvantAttaque) {
+		this.nombreDeToursAvantAttaque = nombreDeToursAvantAttaque;
+	}
+	
     public void subirDegats(int degats) {
         this.pv-=degats;
     }
-
+    
     public boolean estMort() {
-        //si les pv sont inf � 0
-        return this.pv <= 0;
+        //si les pv sont inf � 0 
+    	return this.pv <= 0;
     }
-
-
+    
+    
     public void augmenterXP(int baseExpAdv, int nivAdv) {
         double xpTemporaire = this.xp + (1.5 * nivAdv * baseExpAdv) / 7;
         double seuil = (Math.pow(this.niv, 3) * 0.8);
@@ -112,7 +112,7 @@ public class Pokemon {
         if(this.niv>=espPoke.nivEvolution){
             evoluer();
         }
-        //Les stats de base sont celles de l'espece actuelle du pok�mon.
+        //Les stats de base sont celles de l'espece actuelle du pok�mon. 
         //Ainsi, si le pok�mon a �volu�, son espece a chang� juste avant donc les stats sont calcul�es sur les nouvelles stat de base.
         this.pvMax = (((2*(this.espPoke.pv + this.dvPv)+this.evPv/4)*this.niv ) /100 ) + this.niv + 10;
         this.atk = ((2*(this.espPoke.atq + this.dvAtq)+(evAtq/4)/100)+5);
@@ -124,25 +124,44 @@ public class Pokemon {
         // On modifie uniquement l'espece du pok�mon. Le calcul des nouvelles stat se fait dans augmenterNiv
         this.espPoke=this.espPoke.evolution;
     }
-
+    
     public float obtenirDefSur(String categorieCapa) {
-        if(categorieCapa == "Physique") {
-            return this.def;
-        }else {
-            return this.spe;
-        }
-    }
+    	if(categorieCapa == "Physique") {
+    		return this.def;
+    	}else {
+    		return this.spe;
+    	}
+    }    
     public float obtenirAtqSur(String categorieCapa) {
-        if(categorieCapa == "Physique") {
-            return this.atk;
-        }else {
-            return this.spe;
-        }
+    	if(categorieCapa == "Physique") {
+    		return this.atk;
+    	}else {
+    		return this.spe;
+    	}
     }
-
+    
     public boolean possedeLeType(Type type) {
-        return this.espPoke.type1==type || this.espPoke.type2==type;
+    	return this.espPoke.type1==type || this.espPoke.type2==type;
     }
-
-
+    
+    public void soigner() {
+    	this.pv = this.pvMax;
+    	this.resetPp();
+    }
+    
+    public void resetPp()
+    {
+    	for (Capacite c : this.listeCapacite) {
+    		c.resetPp();
+    	}
+    }
+	public boolean estPlusRapideQue(Pokemon other) {
+		if(this.vit==other.vit) {
+			return Math.random()>0.5;
+		}else {
+			return this.vit>other.vit;
+		}
+	}
+    
+    
 }
