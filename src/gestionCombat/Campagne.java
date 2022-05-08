5d7f9d86c6f10afee7d275e93be5c1efca98f4d7
Firelods <35725423+Firelods@users.noreply.github.com>
2022-05-08 12:20:57 +0200

    campagne tournoi add

---
+++ src/gestionCombat/Campagne.java
@@ -0,0 +1,34 @@
+package gestionCombat;
+
+public class Campagne {
+    public Dresseur joueur;
+    public int nbVictoires;
+
+    Campagne(){
+
+    }
+    public void seConnecter(){
+
+    }
+    public void sInscrire(){
+
+    }
+    public void continuerCampagne(){
+
+    }
+    private void lancerNouveauCombat(){
+
+    }
+    private void genererNouvelAdversaire(){
+
+    }
+    private String choisirTypeAdversaire() {
+        return null;
+    }
+    public void sauvegarder(){
+
+    }
+    public void afficherStat(){
+
+    }
+}
---
+++ src/gestionCombat/Tournoi.java
@@ -0,0 +1,22 @@
+package gestionCombat;
+
+import com.sun.source.tree.BinaryTree;
+
+public class Tournoi {
+    private BinaryTree arbre;
+    private Dresseur[] tabDresseur;
+    private int nbDresseurs;
+    private int nbJoueurs;
+    private int nbIASimples;
+    private int nbIAElaborees;
+
+    public void tourSuivant(){
+
+    }
+    public void combatSuivant(){
+
+    }
+    public void afficherVainqueur(){
+
+    }
+}
