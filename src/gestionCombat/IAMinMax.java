0ee532d5140df34cb6dc313ccb6ce08e7e0abe2d
Arcsti <antoine.arcsti@gmail.com>
2022-06-14 16:06:09 +0200

    Ajout de la classe IAMinMax
    Il faut créer l'objet etatDuJeu
    Ajouter les methodes manquante
    Migrer l'execution d'un tour de Combat vers Tour

---
+++ src/gestionCombat/IAMinMax.java
@@ -0,0 +1,106 @@
+package gestionCombat;
+
+import gestionPokemon.Capacite;
+import gestionPokemon.Pokemon;
+import interfaces.IAttaque;
+import interfaces.ICapacite;
+import interfaces.IDresseur;
+import interfaces.IPokemon;
+
+public class IAMinMax extends Dresseur {
+	
+	private Object[] getProbaVictoire(IPokemon attaquant, IPokemon defenseur, IDresseur dresseurAdv) { 
+		Object[] rep = new Object[2];
+		if(this.estEtatTerminal(attaquant, defenseur, dresseurAdv)) {
+			rep[0] = this.getGain(attaquant, defenseur);
+			rep[1] = null;
+			return rep;
+		}else {
+			int maxi=0;
+			IAttaque[] C1 = this.getCoupsPossibles();
+			IAttaque cmax = C1[0];
+			for(int i=0; i<C1.length;i++) {
+				int mini=1;			
+				IAttaque[] C2 = ((Dresseur) dresseurAdv).getCoupsPossibles();
+				for(int j=0; j<C2.length;j++) {
+					//TODO
+					//On note (P_i,X_i) les états possibles ainsi que leurs probabilités à l'issue des coups choisis
+					Object[] PiXi = null;
+					
+					Object[] etatDuJeu = {this,dresseurAdv};
+					PiXi[0] = etatDuJeu;
+					PiXi[1] = 0;
+							
+					int val=0; //TODO somme_i P_i P(X_i)[0]
+					mini = Math.min(mini,val);
+					maxi = Math.max(mini,maxi);
+				}
+			}
+			rep[0] = maxi;
+			rep[1] = cmax;
+			return rep;
+		}
+	}
+	
+	private int getGain(IPokemon attaquant, IPokemon defenseur) {
+		return 0;
+		
+	}
+	
+	private boolean estEtatTerminal(IPokemon attaquant, IPokemon defenseur,IDresseur dresseurAdv) {
+		return !this.pouvoirSeBattre() || !((Dresseur)dresseurAdv).pouvoirSeBattre();
+	}
+	
+	@Override
+	public IPokemon choisitCombattant() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IAttaque choisitAttaque(IPokemon attaquant, IPokemon defenseur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public IPokemon choisitCombattantContre(IPokemon pok) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	@Override
+	public void enseigne(IPokemon pok, ICapacite[] caps) {
+		Capacite capaciteAApprendre = this.canTeachAMove();
+		if (capaciteAApprendre != null) {
+			if (caps.length < 4) {
+				try {
+					this.getPokemon().remplaceCapacite(caps.length, capaciteAApprendre);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				//System.out.println("\t" + pok.getNom() + " a appris " + capaciteAApprendre.getNom() + " !");
+			} else {
+				//System.out.println("\t" + pok.getNom() + " veut apprendre " + capaciteAApprendre.getNom() + ".");
+				//System.out.println("\tVoulez vous lui faire oublier une des ses capacités (1) ou ne pas l'apprendre (2) ?");
+				int inputChoix = InputViaScanner.getInputInt(1, 2);
+				if (inputChoix == 1) {
+					//((Pokemon) pok).showCapaciteApprise();
+					//System.out.println("\tEntrer le numéro de la capacité à oublier (ou 0 pour annuler) :");
+					int inputCapacite = (int) (Math.random() * caps.length);
+					try {
+						pok.remplaceCapacite(inputCapacite - 1, capaciteAApprendre);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else {
+					//System.out.println("\t" + pok.getNom() + " n'a pas appris " + capaciteAApprendre.getNom() + ".");
+				}
+			}
+		} else {
+			// System.out.println(pok.getNom()+" n'a aucune capacite a apprendre au niveau
+			// "+pok.getNiveau());
+		}
+	}
+}
